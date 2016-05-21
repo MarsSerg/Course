174059bb30cc7695d65ba8771c9a9596b0aac28e
MarsSerg <mydadwasmars@yandex.ru>
2016-05-21 17:00:44 +0300

    FINAL CHAPTER

---
+++ Polzovatel/Kurs/SQLQueryTables.sql
@@ -0,0 +1,115 @@
+insert into dbo.Teacher
+(Teacher_ID,
+Name,
+Surname,
+Lastname)
+Values 
+('111',
+'Sergey',
+'Strokov',
+'Alexeevich')
+
+insert into dbo.Teacher
+(Teacher_ID,
+Name,
+Surname,
+Lastname)
+Values 
+('112',
+'Bruns',
+'Vladislav',
+'Dmitrievich')
+
+insert into dbo.Teacher
+(Teacher_ID,
+Name,
+Surname,
+Lastname)
+Values 
+('113',
+'Ilya',
+'Zadunayski',
+'Sergeevich')
+
+insert into dbo.Anketa
+(ID_Anketa,
+Anketa_name,
+DateOfCreate,
+DateOfLastChange
+)
+Values 
+('1488',
+'Преподаватели',
+  '2016-04-13'  ,
+  Null)
+  
+  insert into dbo.Question
+(ID_Anketa,
+ID_Quest,
+Quest_Text)
+Values 
+('1488',
+'1',
+'Излагает материал ясно и доступно')
+
+  insert into dbo.Question
+(ID_Anketa,
+ID_Quest,
+Quest_Text)
+Values 
+('1488',
+'2',
+'Задает вопросы, побуждает к дискуссии')
+
+insert into dbo.Question
+(ID_Anketa,
+ID_Quest,
+Quest_Text)
+Values 
+('1488',
+'3',
+'Соблюдает логическую последовательность в изложении')
+
+insert into dbo.Question
+(ID_Anketa,
+ID_Quest,
+Quest_Text)
+Values 
+('1488',
+'4',
+'Заинтересованность в успехах студентов')
+
+insert into dbo.Question
+(ID_Anketa,
+ID_Quest,
+Quest_Text)
+Values 
+('1488',
+'5',
+'Творческий интерес и подход к своему делу')
+
+insert into dbo.Question
+(ID_Anketa,
+ID_Quest,
+Quest_Text)
+Values 
+('1488',
+'6',
+'Доброжелательность и такт по отношению к студентам')
+
+insert into dbo.Question
+(ID_Anketa,
+ID_Quest,
+Quest_Text)
+Values 
+('1488',
+'7',
+'Уважительное отношение к студентам')
+
+
+
+
+
+
+
+
